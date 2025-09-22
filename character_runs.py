a772b2e26c312e2c3df8b20f26c9dcb04310f693
SH_note <sanmhyo@gmail.com>
2025-09-22 12:35:23 +0900

    캐릭터 크기조정

--- character_runs.py
+++ character_runs.py
@@ -1,5 +1,6 @@
 from pico2d import *
 
+
 open_canvas()
 
 grass = load_image('grass.png')
@@ -9,7 +10,7 @@ frame = 0
 for x in range(0, 800, 10):
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    character.clip_draw(frame * 100, 0, 100, 100, x, 130, 200, 200)
     update_canvas()
     frame = (frame + 1) % 8
     delay(0.05)
