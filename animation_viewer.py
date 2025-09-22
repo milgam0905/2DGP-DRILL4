046527a3f878d5d8ddb305922348160767bc2361
SH_note <sanmhyo@gmail.com>
2025-09-22 18:52:17 +0900

    수정 편하게 프레임크기 변수 생성

--- animation_viewer.py
+++ animation_viewer.py
@@ -3,18 +3,21 @@ from pico2d import *
 open_canvas()
 img = load_image('skeleton.png')
 # 해당 이미지는 612*576크기
+frame_width = 50  # 프레임 가로 크기
+frame_height = 91  # 프레임 세로 크기
+
 
 def draw_frame(frame = 0, line = 0):
     for x in range(0, 45, 1):
         clear_canvas()
-        img.clip_draw(frame * 50, 576 - line * 91, 50, 100, 400, 300, 300, 300)
+        img.clip_draw(frame * frame_width, 576 - line * frame_height, frame_width, 100, 400, 300, 300, 300)
         update_canvas()
         frame = (frame + 1) % 9
         delay(0.1)
 
 def character_walk():
-
     print("walk")
+    draw_frame(0, 2)
 
 
 
@@ -34,9 +37,8 @@ def character_die():
 
 
 while(1):
-    draw_frame(0,2)
-    # character_walk()
-    # delay(1)
+    character_walk()
+    delay(1)
     # character_attack_1()
     # delay(1)
     # character_attack_2()
