228a3b33c534c8357950a5befbdbf463a7baccf9
SH_note <sanmhyo@gmail.com>
2025-09-22 18:39:26 +0900

    새로운 파일 기본 뼈대 및 무한루프 구현

--- animation_viewer.py
+++ animation_viewer.py
@@ -1,2 +1,30 @@
 from pico2d import *
 
+open_canvas()
+img = load_image('skeleton.png')
+
+def character_walk():
+    pass
+
+
+def character_attack_1():
+    pass
+
+
+def character_attack_2():
+    pass
+
+
+def character_die():
+    pass
+
+
+while(1):
+    character_walk()
+    delay(1)
+    character_attack_1()
+    delay(1)
+    character_attack_2()
+    delay(1)
+    character_die()
+    delay(1)
