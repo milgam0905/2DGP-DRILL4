9d59b6f3a74310577032f511e1d51859c0f5b4d2
SH_note <sanmhyo@gmail.com>
2025-09-22 12:40:11 +0900

    기본 뼈대 및 무한루프 생성

---
+++ animation_viewer.py
@@ -0,0 +1,31 @@
+from pico2d import *
+
+open_canvas()
+img = load_image('sprite_sheet_example.png')
+
+
+def character_run():
+    pass
+
+
+def character_slide():
+    pass
+
+
+def character_jump():
+    pass
+
+
+def character_attak():
+    pass
+
+
+while(1):
+    character_run()
+    delay(1)
+    character_slide()
+    delay(1)
+    character_jump()
+    delay(1)
+    character_attak()
+    delay(1)
