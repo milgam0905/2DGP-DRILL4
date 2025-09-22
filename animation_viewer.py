00d53efe2cadee8579b4f4efd11ae583cbfbfd7d
SH_note <sanmhyo@gmail.com>
2025-09-22 15:21:25 +0900

    프레임을 그리는 부분이 반복되어 함수 생성

--- animation_viewer.py
+++ animation_viewer.py
@@ -8,29 +8,21 @@ frame_width = 50  # 프레임 가로 크기
 frame_height = 37  # 프레임 세로 크기
 top_img = 407 # 이미지의 최대 y좌표
 
-
-def character_run():
-    line = 2
-    frame = 0
+def draw_frame(frame = 0, line = 0):
     for x in range(0, 35, 1):
         clear_canvas()
         img.clip_draw(frame * frame_width, top_img - line * frame_height, frame_width, frame_height, 400, 300, 300, 300)
         update_canvas()
         frame = (frame + 1) % 7
         delay(0.05)
+
+
+def character_run():
+    draw_frame(0,2)
 
 
 def character_slide():
-    print("Character Slide")
-    line = 4
-    frame = 0
-    for x in range(0, 35, 1):
-        clear_canvas()
-        img.clip_draw(frame * frame_width, top_img - line * frame_height, frame_width, frame_height, 400, 300, 300, 300)
-        update_canvas()
-        frame = (frame + 1) % 7
-        delay(0.05)
-    pass
+    draw_frame(0,4)
 
 
 def character_jump():
