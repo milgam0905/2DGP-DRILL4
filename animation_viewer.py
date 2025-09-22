5bfb2d84f11e31dd435365e63f859a92bebeda0c
SH_note <sanmhyo@gmail.com>
2025-09-22 12:49:44 +0900

    무한루픞 및 이미지 확인

--- animation_viewer.py
+++ animation_viewer.py
@@ -5,22 +5,28 @@ img = load_image('sprite_sheet_example.png')
 
 
 def character_run():
+    print("Character Run")
     pass
 
 
 def character_slide():
+    print("Character Slide")
     pass
 
 
 def character_jump():
+    print("Character Jump")
     pass
 
 
 def character_attak():
+    print("Character Attak")
     pass
 
 
 while(1):
+    img.draw(400, 300)
+    update_canvas()
     character_run()
     delay(1)
     character_slide()
